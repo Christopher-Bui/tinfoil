221d53be225794d3584b40083c703ce7cf3ffb67
Chris <christopher.d.bui@gmail.com>
2015-12-26 14:06:46 -0600

    Initial Commit

---
+++ src/tinfoil/core.clj
@@ -0,0 +1,6 @@
+(ns tinfoil.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/tinfoil/core_test.clj
@@ -0,0 +1,7 @@
+(ns tinfoil.core-test
+  (:require [clojure.test :refer :all]
+            [tinfoil.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
