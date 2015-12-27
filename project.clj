1baa7fa9ab1db33792fbdf30b9228e348d3b8891
Chris <christopher.d.bui@gmail.com>
2015-12-27 01:02:21 -0600

    Update repo URL in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject tinfoil "0.1.0-SNAPSHOT"
   :description "Wrap RESTful API's"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/Christopher-Bui/tinfoil"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]])
+                 [org.clojure/core.async "0.2.374"]
+                 [clj-http "2.0.0"]
+                 [http-kit "2.1.18"]])
